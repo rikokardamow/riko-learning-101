bb63b792ed1788a3a1655c1fe9c3c1901189627e
riko Kardamow <rikokardamow@gmail.com>
2026-02-12 06:18:57 +0000

    commit journey for Python Learning!

---
+++ riko's journey.py
@@ -0,0 +1,85 @@
+## Code Comment ##
+# This is a comment ~ way to explain your thinking process!
+# These lines of code will not change any values
+# Anything following the first # is not run as code
+
+""""This is a special string"""
+
+#Code Conventions
+#style familiar to your writing style
+
+#Use Four Spaces for indentation.
+
+if True:
+    x = 5 # Four indentions
+
+#use Lowercase with Underscores for Variable and Function Names.
+#Variables and function names should be in lowercase, with word separated by underscores as necessary to improve readability.
+
+my_variable = 1
+def my_function():
+    pass
+
+#camel cases uses for mainly Class
+
+class MyNewClass():
+    pass
+
+x = 1
+x = x + 1 # Increment X
+
+import os
+import sys
+
+# from third_party import local_module #This is not exist, just for sample code
+
+#Correct
+age = 4 + 18
+
+#Incorrect
+age=4+18
+
+
+def square(number):
+    """"Return the square of the number"""
+    return number * number
+
+square(5)
+
+#Objects,types and Variables
+#int(integer, a whole number with no decimal place)
+#float / String / bool(True/False) / NoneType
+
+num_1 = 10
+num_2 = 2
+num_3 = 6
+
+num_1 + num_2
+
+num_1 - num_3
+
+num_2 * num_2
+
+num_1 / num_3
+
+num_3 ** num_2
+
+num_1 += 2
+num_1
+
+num_1-=2
+num_1
+
+num_3 *=5
+num_3
+
+num_4 = num_1 + num_2 + num_3
+num_4
+
+num_3 != num_4
+
+num_3 > num_2
+
+simple_string_1 = "in example"
+simple_string_1 = simple_string_1 + " we re working"
+simple_string_1
